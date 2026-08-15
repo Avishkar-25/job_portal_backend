ff9ca8607910492a1b02d551039fa4fdc949bebe
Avishkar-25 <avishkarghadge25@gmail.com>
2026-08-15 22:14:57 +0530

    Fix localhost CORS

--- server.js
+++ server.js
@@ -10,42 +10,10 @@ const app = express();
 // CORS
 // =====================================================
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://job-portal-frontend.vercel.app",
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests without Origin
-      // Example: Postman / server-to-server
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-
+    origin: "http://localhost:5173",
     credentials: true,
-
-    methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "DELETE",
-      "PATCH",
-      "OPTIONS",
-    ],
-
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-    ],
   })
 );
 
