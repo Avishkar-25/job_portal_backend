8f66e2727d7e607f91b44e4272cfae4847a0511f
Avishkar-25 <avishkarghadge25@gmail.com>
2026-08-15 15:05:21 +0530

    Fix employee job controller filename case

--- config/db.js
+++ config/db.js
@@ -1,28 +1,19 @@
-const mysql=require("mysql2");
+const mysql = require("mysql2");
 
-
-const db=mysql.createConnection({
-
-host:process.env.DB_HOST,
-user:process.env.DB_USER,
-password:process.env.DB_PASSWORD,
-database:process.env.DB_NAME
-
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-
-db.connect((err)=>{
-
-if(err)
-{
-console.log(err);
-}
-else
-{
-console.log("MySQL Connected");
-}
-
+db.connect((err) => {
+    if (err) {
+        console.error("MySQL Connection Error:", err);
+    } else {
+        console.log("MySQL Connected Successfully");
+    }
 });
 
-
-module.exports=db;
+module.exports = db;
